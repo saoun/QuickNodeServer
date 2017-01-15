8f976f1134aff6e9999bd32cd916792eb24312be
sarah aoun <sarah@silk.co>
2017-01-15 15:06:19 -0500

    fix server file

--- QuickNodeServer.sh
+++ QuickNodeServer.sh
@@ -28,30 +28,29 @@ function node_touch(){
           echo -e '"use strict";\n(function(){\n\n})();' > client/public/javascript/script.js
     # mkdir server
     mkdir server
-      printf
-      'var express = require("express");
-      var app = express();
-      var mustache = require("mustache-express");
+      printf  'var express = require("express");
+var app = express();
+var mustache = require("mustache-express");
 
-      app.engine("html",mustache());
-      app.set("view engine", "html");
-      app.set("views", __dirname+"/../client/views");
-      app.use("/public", express.static(__dirname+"/../client/public"));
+app.engine("html",mustache());
+app.set("view engine", "html");
+app.set("views", __dirname+"/../client/views");
+app.use("/public", express.static(__dirname+"/../client/public"));
 
-      //Define the port
-      var port = 8080;
+//Define the port
+var port = 8080;
 
-      //Define what happens then a user visits the root route
-      app.get("/",function(req,res)
-      {
-        res.render("index"); //Tell Express which html file to render for this route
-      });
+//Define what happens then a user visits the root route
+app.get("/",function(req,res)
+{
+  res.render("index"); //Tell Express which html file to render for this route
+});
 
-      //Start the server on the defined port
-      app.listen(port, function()
-      {
-        console.log("Server running on port: "+port);
-      })' > server/index.js
+//Start the server on the defined port
+app.listen(port, function()
+{
+  console.log("Server running on port: "+port);
+})' > server/index.js
 
     npm init
     npm install express --save
