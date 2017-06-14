346439e1fc526f16cafc5d9dd4facfd0b6811d8c
Jeff <joelle_van_dyne@joelle-van-dyne.local>
2017-06-14 10:38:01 -0400

    improved exit command, removed server directory, added start script to package.json

--- QuickNodeServer.sh
+++ QuickNodeServer.sh
@@ -1,11 +1,12 @@
 function node_touch(){
+
   if [ "$#" -ne 1 ]
   then
     echo "You must provide a project name. Usage: 'node_touch [name]'"
-    return
+    exit 0
   fi
     mkdir $1
-    cd $1
+    cd !$
     mkdir client
       mkdir client/views
         echo -e '<!doctype html>\n<html>\n<head>\n  <link href="stylesheets/style.css" rel="stylesheet" type="text/css">\n</head>\n<body>\n  <div class="container">\n    "Hello World."\n  </div>\n</body>\n</html>\n<script src="javascript/jquery.js" type="text/javascript"></script>\n<script src="javascript/script.js" type="text/javascript"></script>' > client/views/index.html
@@ -26,35 +27,37 @@ function node_touch(){
           fi
 
           echo -e '"use strict";\n(function(){\n\n})();' > client/public/javascript/script.js
-    # mkdir server
-    mkdir server
+
       printf  'var express = require("express");
-var app = express();
-var mustache = require("mustache-express");
+  var app = express();
+  var mustache = require("mustache-express");
 
-app.engine("html",mustache());
-app.set("view engine", "html");
-app.set("views", __dirname+"/../client/views");
-app.use("/", express.static(__dirname+"/../client/public"));
+  app.engine("html",mustache());
+  app.set("view engine", "html");
+  app.set("views", __dirname+"/../client/views");
+  app.use("/", express.static(__dirname+"/../client/public"));
 
-//Define the port
-var port = 8080;
+  //Define the port
+  var port = 8080;
 
-//Define what happens then a user visits the root route
-app.get("/",function(req,res)
-{
+  //Define what happens then a user visits the root route
+  app.get("/",function(req,res)
+  {
   res.render("index"); //Tell Express which html file to render for this route
-});
+  });
 
 //Start the server on the defined port
 app.listen(port, function()
 {
   console.log("Server running on port: "+port);
-})' > server/index.js
+})' > index.js
 
     npm init
     npm install express --save
     npm install mustache-express --save
     npm install nodemon --save
+    npm install -g json # installs a global NPM package that will allow us to edit package.json easier
+    json -I -f package.json -e 'this.scripts.start = "nodemon index.js"' # adds start script to package.json
     subl . #change this line to your editor of choice.
+
 }
