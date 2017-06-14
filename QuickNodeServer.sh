a82f7cd99a265b81ecd28f053b61cbafbc2ce64a
Jeff <joelle_van_dyne@joelle-van-dyne.local>
2017-06-14 11:06:08 -0400

    undo indentation changes

--- QuickNodeServer.sh
+++ QuickNodeServer.sh
@@ -29,22 +29,22 @@ function node_touch(){
           echo -e '"use strict";\n(function(){\n\n})();' > client/public/javascript/script.js
 
       printf  'var express = require("express");
-  var app = express();
-  var mustache = require("mustache-express");
+var app = express();
+var mustache = require("mustache-express");
 
-  app.engine("html",mustache());
-  app.set("view engine", "html");
-  app.set("views", __dirname+"/../client/views");
-  app.use("/", express.static(__dirname+"/../client/public"));
+app.engine("html",mustache());
+app.set("view engine", "html");
+app.set("views", __dirname+"/../client/views");
+app.use("/", express.static(__dirname+"/../client/public"));
 
-  //Define the port
-  var port = 8080;
+//Define the port
+var port = 8080;
 
-  //Define what happens then a user visits the root route
-  app.get("/",function(req,res)
-  {
-  res.render("index"); //Tell Express which html file to render for this route
-  });
+//Define what happens then a user visits the root route
+app.get("/",function(req,res)
+{
+res.render("index"); //Tell Express which html file to render for this route
+});
 
 //Start the server on the defined port
 app.listen(port, function()
